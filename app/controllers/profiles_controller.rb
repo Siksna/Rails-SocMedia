9c3a8650b9c8a1e6eb216c13505a3d78ef746712
Ulfs Siksna <ulfsssiksna@gmail.com>
2024-10-02 17:25:56 +0300

    Izveidots saraksts lietotaja profilā kur tiek parādītas visas ziņas ko lietotājs ir veicis

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,12 @@
 class ProfilesController < ApplicationController
 
     def show
-        @user = User.find(params[:id])
+        
+              @user = User.find(params[:id])
+              @posts = @user.messages.order(created_at: :desc)
+              @replies = @user.replies.order(created_at: :desc)
+          
+          
     end
 
 end
